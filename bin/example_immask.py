5f90b9138824cc397af63b49ae52d8c5e6db3dcf
Felipe Menanteau <felipe@illinois.edu>
2014-01-30 18:00:36 +0000

    Cleaned up example

--- bin/example_immask.py
+++ bin/example_immask.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python
 
 import time
-import os,sys
+import os
+import sys
 import immask
 
 # Get the start time
@@ -11,29 +12,9 @@ args   = immask.cmdline()
 desobj = immask.DESIMA(args.fileName,args.outdir)
 
 # CR Rejection
-#desobj.CRs(FWHM     = args.fwhm,
-#           dilateCR = args.dilateCR,
-#           interpCR = args.interpCR,
-#           minSigma = args.minSigma,
-#           min_DN   = args.min_DN,
-#           nGrowCR  = args.nGrowCR)
-
-streak_args = {
-    'bkgfile'    :'DECam_00145879_21_bkg.fits.fz',
-    'bin_factor' : 8,
-    }
-streakobj = immask.STREAK(desobj)
-streakobj.mask_streaks(**streak_args)#,writeobjs=True,writemask=False,draw=False) 
-#streakobj.draw()
-#desobj.OUT_MSK = msk
-#desobj.OUT_WGT = wgt
-# Write it out
+desobj.CRs(**args.__dict__)
+desobj.mask_streaks(**args.__dict__)
 desobj.write(compress=args.compress)
 print >>sys.stderr,"# Time:%s" % immask.elapsed_time(t0)
-
-
-sys.exit()
-
-
 
 
