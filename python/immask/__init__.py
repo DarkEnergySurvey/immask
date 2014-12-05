07987472d0dbc2bec13356bf16453433d487392d
Alex Drlica-Wagner <kadrlica@fnal.gov>
2014-12-05 00:37:16 +0000

    Parse SVN url to version string

--- python/immask/__init__.py
+++ python/immask/__init__.py
@@ -2,29 +2,18 @@
 The DESDM single-CCD image masking module.
 
 $Id: __init__.py 22391 2014-05-29 22:17:54Z kadrlica $
-$Rev::                                  $:  # Revision of last commit.
+$Rev:: 30138                            $:  # Revision of last commit.
 $LastChangedBy:: kadrlica               $:  # Author of last commit.
 $LastChangedDate:: 2014-05-29 17:17:54 #$:  # Date of last commit.
 """
+svnrev = "$Rev:$"
+svnurl = "$HeadURL$"
 
 __author__  = "Felipe Menanteau, Alex Drlica-Wagner, Eli Rykoff"
-__version__ = 'trunk'
-__revision__= "$Rev$".strip('$').split()[-1]
+__version__ = svnurl.strip('$').split()[-1].split(os.sep)[-4]
+__revision__= svnrev.strip('$').split()[-1]
 version = __version__
 
 from . import immasklib
 from .immasklib import cmdline
 from .immasklib import elapsed_time
-
-#from .immasklib import DESIMA
-
-# Other ways... not really used
-# CR masking routine only
-#from cr_masking import *
-
-# To load the standalone streak masking routines at startup --- not required
-#from . import streak_masking
-#from .streak_masking import STREAK
-#from .streak_masking import cmdline
-#print "# Module %s v%s is loaded" % (__name__,__version__)
-
