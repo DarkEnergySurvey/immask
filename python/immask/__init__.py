bac221ea89a44e3d6496867e2e9783da3f12e7fd
Alex Drlica-Wagner <kadrlica@fnal.gov>
2014-12-13 01:56:07 +0000

    Updated svn:keywords

--- python/immask/__init__.py
+++ python/immask/__init__.py
@@ -2,11 +2,13 @@
 The DESDM single-CCD image masking module.
 
 $Id: __init__.py 22391 2014-05-29 22:17:54Z kadrlica $
-$Rev:: 30138                            $:  # Revision of last commit.
+$Rev::                                  $:  # Revision of last commit.
 $LastChangedBy:: kadrlica               $:  # Author of last commit.
 $LastChangedDate:: 2014-05-29 17:17:54 #$:  # Date of last commit.
 """
-svnrev = "$Rev:$"
+import os
+
+svnrev = "$Rev$"
 svnurl = "$HeadURL$"
 
 __author__  = "Felipe Menanteau, Alex Drlica-Wagner, Eli Rykoff"
