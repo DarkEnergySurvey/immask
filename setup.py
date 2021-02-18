0d2a2157d7308f1d2d28f1e1197efae9f841a6e3
Felipe Menanteau <felipe@illinois.edu>
2021-02-18 11:32:18 -0600

    Fixes to vertices

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 
 # The main call
 setup(name='immask',
-      version ='3.0.0',
+      version ='3.0.1',
       license = "GPL",
       description = "The Image Masking Module for DESDM.",
       author = "Felipe Menanteau, Alex Drlica-Wagner, Eli Rykoff",
